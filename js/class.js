bfc39c5f39bf51bcd40ebce589f13a1aeb73e242
Karl97490 <karl.pery@icloud.com>
2026-05-17 18:33:37 +0400

    feat: build a setNeighbors() method in the Cell class

--- js/class.js
+++ js/class.js
@@ -3,6 +3,7 @@ class Cell {
         this.positionX = positionX; // Store the horizontal position of the cell instance 
         this.positionY = positionY; // Store the vertical position of the cell instance
         this.state = 0; // Represents the state of a cell (alive or dead)
+        this.neighbors = []; // Store the cell’s neighbors
 
         this.updateUI()
     }
@@ -16,11 +17,131 @@ class Cell {
 
         this.newCell.addEventListener("click", () => { // Create an event listener for each div element
             this.newCell.classList.toggle("alive") // When a case (cell) is clicked, toggle the 'alive' class
-            if(this.state){ // Check the state of the cell
-                this.state = 0; // If alive, switch it state to 0 (dead)
-            }else{
-                this.state = 1; // If dead, switch it state to 1 (alive)
-            }
+
+            // Check the state of the cell
+            this.state = this.state ? 0 : 1 // If alive, switch its state to 0 (dead), if dead switch it to 1 (alive)
         })
     }
+
+    setNeighbors() {
+
+        // Differentiate the cells based on their position in the grid
+
+        // For cells in the corners :
+        // Top left
+        if (this.positionX === 0 && this.positionY === 0) {
+            console.log("I'm from the top-left corner...")
+            // Push the neighbors to the neighbors array
+            if (!this.neighbors.length) {  // Check if array is empty
+                this.neighbors.push(
+                    cellMatrix[this.positionX][this.positionY + 1], // Right
+                    cellMatrix[this.positionX + 1][this.positionY + 1], // Diagonal 
+                    cellMatrix[this.positionX + 1][this.positionY] // Below
+                )
+            }
+        }
+        // Bottom left
+        else if (this.positionX === 59 && this.positionY === 0) {
+            console.log("I'm from the bottom-left corner...")
+            if (!this.neighbors.length) {  // Check if array is empty
+                this.neighbors.push(
+                    cellMatrix[this.positionX][this.positionY + 1], // Right
+                    cellMatrix[this.positionX - 1][this.positionY + 1], // Diagonal
+                    cellMatrix[this.positionX - 1][this.positionY] // Above
+                )
+            }
+        }
+        // Top right
+        else if (this.positionX === 0 && this.positionY === 59) {
+            console.log("I'm from the top-right corner...")
+            if (!this.neighbors.length) {  // Check if array is empty
+                this.neighbors.push(
+                    cellMatrix[this.positionX][this.positionY - 1], // Left
+                    cellMatrix[this.positionX + 1][this.positionY - 1], // Diagonal
+                    cellMatrix[this.positionX + 1][this.positionY] // Below
+                )
+            }
+        }
+        // Bottom right
+        else if (this.positionX === 59 && this.positionY === 59) {
+            console.log("I'm from the bottom-right corner...")
+            if (!this.neighbors.length) {  // Check if array is empty
+                this.neighbors.push(
+                    cellMatrix[this.positionX][this.positionY - 1], // Left
+                    cellMatrix[this.positionX - 1][this.positionY - 1], // Diagonal
+                    cellMatrix[this.positionX - 1][this.positionY] // Above 
+                )
+            }
+        }
+
+        // For cells on the borders (excluding the corners) :
+        // Top border
+        else if (this.positionX === 0 && this.positionY >= 1 && this.positionY <= 58) {
+            console.log("I'm from the top border...")
+            if (!this.neighbors.length) {  // Check if array is empty
+                this.neighbors.push(
+                    cellMatrix[this.positionX][this.positionY - 1], // Left
+                    cellMatrix[this.positionX + 1][this.positionY - 1], // Diagonal-left
+                    cellMatrix[this.positionX + 1][this.positionY], // Below
+                    cellMatrix[this.positionX + 1][this.positionY + 1], // Diagonal-right
+                    cellMatrix[this.positionX][this.positionY + 1], // Right
+                )
+            }
+        }
+        // Right border
+        else if (this.positionX >= 1 && this.positionX <= 58 && this.positionY === 59) {
+            console.log("I'm from the right border...")
+            if (!this.neighbors.length) {  // Check if array is empty
+                this.neighbors.push(
+                    cellMatrix[this.positionX - 1][this.positionY], // Above
+                    cellMatrix[this.positionX - 1][this.positionY - 1], // Diagonal-above
+                    cellMatrix[this.positionX][this.positionY - 1], // Left
+                    cellMatrix[this.positionX + 1][this.positionY - 1], // Diagonal-below
+                    cellMatrix[this.positionX + 1][this.positionY], // Below
+                )
+            }
+        }
+        // Bottom border
+        else if (this.positionX === 59 && this.positionY >= 1 && this.positionY <= 58) {
+            console.log("I'm from the bottom border...")
+            if (!this.neighbors.length) {  // Check if array is empty
+                this.neighbors.push(
+                    cellMatrix[this.positionX][this.positionY - 1], // Left
+                    cellMatrix[this.positionX - 1][this.positionY - 1], // Diagonal-left
+                    cellMatrix[this.positionX - 1][this.positionY], // Above
+                    cellMatrix[this.positionX - 1][this.positionY + 1], // Diagonal-right
+                    cellMatrix[this.positionX][this.positionY + 1], // Right
+                )
+            }
+        }
+        // Left border
+        else if (this.positionX >= 1 && this.positionX <= 58 && this.positionY === 0) {
+            if (!this.neighbors.length) {  // Check if array is empty
+                this.neighbors.push(
+                    cellMatrix[this.positionX - 1][this.positionY], // Above
+                    cellMatrix[this.positionX - 1][this.positionY + 1], // Diagonal-above
+                    cellMatrix[this.positionX][this.positionY + 1], // Right
+                    cellMatrix[this.positionX + 1][this.positionY + 1], // Diagonal-below
+                    cellMatrix[this.positionX + 1][this.positionY], // Below
+                )
+            }
+        }
+        // For all other cells excluding corners and border cells :
+        else {
+            if (!this.neighbors.length) {  // Check if array is empty
+                this.neighbors.push(
+                    cellMatrix[this.positionX - 1][this.positionY], // Above
+                    cellMatrix[this.positionX - 1][this.positionY + 1], // Diagonal-right-above
+                    cellMatrix[this.positionX][this.positionY + 1], // Right
+                    cellMatrix[this.positionX + 1][this.positionY + 1], // Diagonal-right-below
+                    cellMatrix[this.positionX + 1][this.positionY], // Below
+                    cellMatrix[this.positionX + 1][this.positionY - 1], // Diagonal-left-below
+                    cellMatrix[this.positionX][this.positionY - 1], // Left
+                    cellMatrix[this.positionX - 1][this.positionY - 1], // Diagonal-left-above
+                )
+            }
+        }
+
+    }
+
 }
