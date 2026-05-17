9c9f4ce45cbd5f61df82927389b24674292510e2
Karl97490 <karl.pery@icloud.com>
2026-05-17 15:03:44 +0400

    feat: user can activate/desactivate a cell

--- js/class.js
+++ js/class.js
@@ -2,6 +2,7 @@ class Cell {
     constructor(positionX, positionY) {
         this.positionX = positionX; // Store the horizontal position of the cell instance 
         this.positionY = positionY; // Store the vertical position of the cell instance
+        this.state = 0; // Represents the state of a cell (alive or dead)
 
         this.updateUI()
     }
@@ -15,6 +16,11 @@ class Cell {
 
         this.newCell.addEventListener("click", () => { // Create an event listener for each div element
             this.newCell.classList.toggle("alive") // When a case (cell) is clicked, toggle the 'alive' class
+            if(this.state){ // Check the state of the cell
+                this.state = 0; // If alive, switch it state to 0 (dead)
+            }else{
+                this.state = 1; // If dead, switch it state to 1 (alive)
+            }
         })
     }
 }
